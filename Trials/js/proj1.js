bb43b16e2586694349edc165e59e60cad48c8108
Jun.Nishiguchi <Jun.Nishiguchi@e86a3ad0-1d0d-11df-a75e-e1da74a5f56a>
2010-02-25 00:57:29 +0000

    Project 1 fixes

--- Trials/js/proj1.js
+++ Trials/js/proj1.js
@@ -8,14 +8,3 @@ function change()
 	document.getElementById("VW").style.height=document.getElementById("text1").value + "px";
 	document.getElementById("VW").style.width=document.getElementById("text2").value + "px";
 }
-
-function gridFormation()
-{
-	for (j=1;j<=16;j++)
-	{
-		i=document.createElement("img");
-		i.height=patchSize/2;
-		i.width=patchSize/2;
-		document.getElementById("patchGrid").appendChild(i);
-	}
-}
